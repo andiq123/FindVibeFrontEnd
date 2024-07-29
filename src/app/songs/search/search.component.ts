e971c71b83dda796b6914ed3e144301a43f1f662
Andi <andy.u789@gmail.com>
2024-07-29 22:19:27 +0300

    chore: Update search component with suggestions dropdown

--- src/app/songs/search/search.component.ts
+++ src/app/songs/search/search.component.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { SongsService } from '../songs.service';
 import { SuggestionsService } from '../suggestions.service';
 import { TitleCasePipe } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -22,11 +24,19 @@ export class SearchComponent implements OnInit {
   timeOut: ReturnType<typeof setTimeout> | undefined;
   constructor(
     private songsService: SongsService,
-    private suggestionsService: SuggestionsService
+    private suggestionsService: SuggestionsService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.suggestions = this.suggestionsService.suggestions$;
+    this.activatedRoute.queryParams.pipe(take(2)).subscribe((params) => {
+      if (params['query']) {
+        this.searchTerm.set(params['query']);
+        this.submit();
+      }
+    });
   }
 
   addSuggestion(suggestion: string) {
@@ -38,6 +48,11 @@ export class SearchComponent implements OnInit {
   search(event: Event) {
     const value = (event.target as HTMLInputElement).value;
     this.searchTerm.set(value);
+
+    this.router.navigate([], {
+      queryParams: { query: value },
+      queryParamsHandling: 'merge',
+    });
 
     if (this.timeOut) {
       clearTimeout(this.timeOut);
@@ -54,6 +69,7 @@ export class SearchComponent implements OnInit {
 
   submit() {
     if (this.searchTerm() === '') return;
+    this.suggestionsService.reset();
     this.songsService.searchSongs(this.searchTerm()).subscribe();
   }
 }
