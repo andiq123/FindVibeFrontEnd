a282412c917652756a105de48a907e54ae243d71
Andi <andy.u789@gmail.com>
2024-08-08 20:55:48 +0300

    asdqwdas

--- src/app/components/player-wrapper/directives/swipe-down.directive.ts
+++ src/app/components/player-wrapper/directives/swipe-down.directive.ts
@@ -46,8 +46,8 @@ export class SwipeDownDirective {
 
   @HostListener('touchend', ['$event'])
   onSwipeEnd() {
-    const closeSizeTrigger = 350;
-    const timeCloseTrigger = 500;
+    const closeSizeTrigger = 400;
+    const timeCloseTrigger = 600;
     const duration = new Date().getTime() - this.startTime().getTime();
 
     if (duration < timeCloseTrigger && this.topSignal() > closeSizeTrigger) {
