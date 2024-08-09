8508168eb70e30e2f61c54ce24180b180ffdfedb
Andi <andy.u789@gmail.com>
2024-08-09 14:25:30 +0300

    asdqwdasd

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,31 +1,36 @@
-import {Component, computed, signal} from '@angular/core';
-import {FaIconComponent, IconDefinition} from "@fortawesome/angular-fontawesome";
-import {faList, faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
-import {TitleCasePipe} from "@angular/common";
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import { Component, computed, signal } from '@angular/core';
+import {
+  FaIconComponent,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faFolderOpen,
+  faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons';
+import { TitleCasePipe } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [
-    FaIconComponent,
-    TitleCasePipe,
-    RouterLink,
-    RouterLinkActive
-  ],
+  imports: [FaIconComponent, TitleCasePipe, RouterLink, RouterLinkActive],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
-  navListSource = computed(() => [{
-    name: 'search',
-    icon: faMagnifyingGlass,
-    link: '/songs'
-  },
+  navListSource = computed(() => [
+    {
+      name: 'search',
+      icon: faMagnifyingGlass,
+      link: '/songs',
+    },
     {
       name: 'library',
-      icon: faList,
-      link: '/library'
-    }]);
-  navList = signal<{ name: string, icon: IconDefinition, link: string }[]>(this.navListSource());
+      icon: faFolderOpen,
+      link: '/library',
+    },
+  ]);
+  navList = signal<{ name: string; icon: IconDefinition; link: string }[]>(
+    this.navListSource()
+  );
 }
