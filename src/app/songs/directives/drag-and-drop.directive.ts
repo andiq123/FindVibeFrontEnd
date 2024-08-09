a3e51c465ce99f5a58e90eef80b634f752f87517
Andi <andy.u789@gmail.com>
2024-08-10 00:01:31 +0300

    sadqwdasd

--- src/app/songs/directives/drag-and-drop.directive.ts
+++ src/app/songs/directives/drag-and-drop.directive.ts
@@ -66,13 +66,10 @@ export class DragAndDropDirective {
     const elToRemove = target.querySelector('#elToRemove');
     elToRemove?.classList.remove('pointer-events-none');
 
-    const draggable = target.attributes.getNamedItem('draggable');
-    if (draggable) {
-      target.classList.remove('dragTarget');
-      const initiatorId = event.dataTransfer?.getData('id');
-      const title = target.id;
-      this.reorderSongs.emit({ from: initiatorId!, to: title });
-    }
+    target.classList.remove('dragTarget');
+    const initiatorId = event.dataTransfer?.getData('id');
+    const title = target.id;
+    this.reorderSongs.emit({ from: initiatorId!, to: title });
   }
 
   @HostListener('dragover', ['$event'])
