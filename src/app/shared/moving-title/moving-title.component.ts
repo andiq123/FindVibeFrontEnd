a08a06ffdda757918fa6bd26e2cfb485cb63660c
Andi <andy.u789@gmail.com>
2024-08-10 20:06:04 +0300

    asdqwdsad

--- src/app/shared/moving-title/moving-title.component.ts
+++ src/app/shared/moving-title/moving-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input, output, signal } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
