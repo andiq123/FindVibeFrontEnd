391bb77800cb3c65076c640533dd484f8cae6fa0
andi <andy.u789@gmail.com>
2026-07-26 15:00:22 +0300

    asdad

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: "http://localhost:8080",
-  isDebug: false
+  API_URL: "http://192.168.100.161:8080",
+  isDebug: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: "http://localhost:8080",
+  API_URL: "http://192.168.100.161:8080",
   isDebug: false
 };
